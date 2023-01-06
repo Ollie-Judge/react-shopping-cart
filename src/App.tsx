1d138a1c042eefea329899019f2935da7eff0ff1
Oliver Judge <oliver.judge@guestline.com>
2023-01-06 11:29:41 +0000

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./Pages/Home";
+import { Store } from "./Pages/Store";
+import { About } from "./Pages/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+        <h1>Test</h1>
+      </Container>
     </div>
   );
 }
